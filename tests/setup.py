f5924fd433f2645ffc1af3897b7e0c78933a7de0
Hang Su <hang.su@algorand.com>
2022-02-10 10:49:08 -0500

    setup test env config

---
+++ tests/setup.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class TestEnvConfig:
+    ALGOD_ADDRESS: str
+    ALGOD_TOKEN: str
+    KMD_ADDRESS: str
+    KMD_TOKEN: str
+    KMD_WALLET_NAME: str
+    KMD_WALLET_PASSWORD: str
+
+
+SandBoxTestEnvConfig = TestEnvConfig(
+    ALGOD_ADDRESS="http://localhost:4001",
+    ALGOD_TOKEN="a" * 64,
+    KMD_ADDRESS="http://localhost:4002",
+    KMD_TOKEN="a" * 64,
+    KMD_WALLET_NAME="unencrypted-default-wallet",
+    KMD_WALLET_PASSWORD="",
+)
+
+# TODO needs configuration for betanet, but we now work over sandbox settings
+# NOTE some thoughts about how this is done for general betanet / any other networks
