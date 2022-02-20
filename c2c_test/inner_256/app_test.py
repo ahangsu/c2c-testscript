a1f672d397e1f6928751b764873e82cc7f668e70
Hang Su <hang.su@algorand.com>
2022-02-20 11:31:12 -0500

    inner 256

--- c2c_test/inner_256/app_test.py
+++ c2c_test/inner_256/app_test.py
@@ -91,30 +91,31 @@ def test_algod(context: EnvSetupDict):
     context.algod_client.send_transactions(context.stxns)
     results = [wait_for_confirmation(context.algod_client, txid, 4) for txid in txids]
     result = results[1]
+    context.txid = txids[1]
 
     assert "logs" not in result
     assert len(result["inner-txns"]) == MAX_INNER_CALL_COUNT
     for i in range(MAX_INNER_CALL_COUNT):
         assert len(result["inner-txns"][i]["logs"]) == 1
         substr_log = str(i) + " time(s) called, called by"
-        log_decoded = base64.b64decode(result["inner-txns"][i]["logs"][0]).decode(
-            "utf-8"
-        )
+        log_bytes = result["inner-txns"][i]["logs"][0]
+        log_decoded = base64.b64decode(log_bytes).decode("utf-8")
         assert substr_log in log_decoded
 
 
 def test_indexer_validation(context: EnvSetupDict):
     time.sleep(5)
-    var = context.indexer_client.search_transactions_by_address(
+    full_txn = context.indexer_client.search_transactions_by_address(
         address=context.addr, txid=context.txid
     )
-    print(var)
-    top_level_log = context.indexer_client.application_logs(
-        context.app_id_1st, txid=context.txid, sender_addr=context.addr
-    )
-    some_inner_log = context.indexer_client.application_logs(
-        context.app_id_2nd, txid=context.txid, sender_addr=context.app_addr_1st
-    )
-    print(top_level_log)
-    print(some_inner_log)
-    pass
+    assert "logs" not in full_txn
+    assert len(full_txn["transactions"]) == 1
+    assert "logs" not in full_txn["transactions"][0]
+    assert len(full_txn["transactions"][0]["inner-txns"]) == MAX_INNER_CALL_COUNT
+    for i in range(MAX_INNER_CALL_COUNT):
+        log_ith_index = full_txn["transactions"][0]["inner-txns"][i]["logs"]
+        assert len(log_ith_index) == 1
+        log_bytes = log_ith_index[0]
+        substr_log = str(i) + " time(s) called, called by"
+        log_decoded = base64.b64decode(log_bytes).decode("utf-8")
+        assert substr_log in log_decoded
